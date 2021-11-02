ea128fcfb5b27a1536fe844c53f18c83db46de1a
kazuki0824 <kazukuro0824@gmail.com>
2021-11-03 02:21:29 +0900

    Mirakurunセットアップ(設定ファイルの配置)を飛ばしていたので追加

--- setup.sh
+++ setup.sh
@@ -18,3 +18,4 @@ cp epgstation/config/config.yml.template epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
 cp epgstation/config/serviceLogConfig.sample.yml epgstation/config/serviceLogConfig.yml
+docker-compose run --rm -e SETUP=true mirakurun
