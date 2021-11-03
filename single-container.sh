b08e5860a562331dc92b846f3d724017636c7fd7
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2021-11-04 02:00:47 +0900

    Update single-container.sh

--- single-container.sh
+++ single-container.sh
@@ -4,7 +4,7 @@ git clone https://github.com/kazuki0824/EPGStation-nvenc-docker.git --no-checkou
 
 cd EPGStation-nvenc-docker
 git sparse-checkout init
-git sparse-checkout set epgstation
+git sparse-checkout set epgstation-nvenc
 git checkout master
 
 DOCKER_BUILDKIT=1 docker build ./epgstation-nvenc/
