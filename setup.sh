4d26533a595c7651b286cf1ee3b8ed08cf9fe2c6
Yui Maruyama <16yuim@gmail.com>
2021-11-11 13:48:35 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/l3tnun/docker-mirakurun-epgstation.git
 cd docker-mirakurun-epgstation
 curl -O https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/docker-compose.yml
-curl -o epgstation/custom.Dockerfile https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/epgstation-nvenc/Dockerfile
+curl -o epgstation/custom.Dockerfile https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/epgstation-nvenc/custom.Dockerfile
 curl -o epgstation/config/config.yml https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/epgstation-nvenc/config/config.yml.template
 docker-compose run --rm -e SETUP=true mirakurun
