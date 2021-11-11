7e2204af99e7e40e901e2a8b515db66693408a99
Yui Maruyama <16yuim@gmail.com>
2021-11-11 13:57:33 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ git pull
 curl -O https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/docker-compose.yml
 curl -o epgstation/custom.Dockerfile https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/epgstation-nvenc/Dockerfile
 curl -o epgstation/config/config.yml https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/epgstation-nvenc/config/config.yml.template
+docker-compose pull && docker-compose up -d
