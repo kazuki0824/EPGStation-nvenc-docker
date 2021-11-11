9409ffa1ec7a91cd8ca9d43c9782b7cea49cfab2
Yui Maruyama <16yuim@gmail.com>
2021-11-11 13:37:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/kazuki0824/EPGStation-nvenc-docker.git
-INSTALLDIR=$PWD/EPGStation-nvenc-docker
-
-cd /tmp
 git clone https://github.com/l3tnun/docker-mirakurun-epgstation.git
 cd docker-mirakurun-epgstation
-rm -rf ./.gi*
-cp -r ./epgstation $INSTALLDIR/.epgstation -n
-cp ./docker-compose-sample.yml $INSTALLDIR/docker-compose-no-nvenc.yml
-cd ..
-rm -rf docker-mirakurun-epgstation
-
-cd $INSTALLDIR
-cp .epgstation/config/enc.js.template epgstation-nvenc/config/enc.js
-cp epgstation-nvenc/config/config.yml.template epgstation-nvenc/config/config.yml
-cp .epgstation/config/operatorLogConfig.sample.yml epgstation-nvenc/config/operatorLogConfig.yml
-cp .epgstation/config/epgUpdaterLogConfig.sample.yml epgstation-nvenc/config/epgUpdaterLogConfig.yml
-cp .epgstation/config/serviceLogConfig.sample.yml epgstation-nvenc/config/serviceLogConfig.yml
+curl -O https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/docker-compose.yml
+curl -o epgstation/custom.Dockerfile https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/epgstation-nvenc/Dockerfile
+curl -o epgstation/config/config.yml https://raw.githubusercontent.com/kazuki0824/EPGStation-nvenc-docker/main/epgstation-nvenc/config/config.yml.template
 docker-compose run --rm -e SETUP=true mirakurun
